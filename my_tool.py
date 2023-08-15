6cd32f15264a45575fadd14e8c9165854385369d
thuannd99 <thuannguyen@remitano.com>
2023-08-15 16:31:18 +0700

    Init tool

---
+++ my_tool.py
@@ -0,0 +1,16 @@
+from superagi.tools.base_tool import BaseTool
+from pydantic import BaseModel, Field
+from typing import Type
+
+class MyToolInput(BaseModel):
+    message: str = Field(..., description="Greeting message to be sent")
+
+class MyTool(BaseTool):
+    name: str = "My Tool"
+    args_schema: Type[BaseModel] = MyToolInput
+    description: str = "Sends a Greeting Message"
+
+    def _execute(self, greetings: str = None):
+        from_name = self.get_tool_config('FROM')
+        greetings_str = greetings + "\n" + from_name
+        return greetings_str
