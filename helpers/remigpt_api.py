4492cb9f682574b12577ddfcfcd7dd182d72b81a
thuannd99 <thuannguyen@remitano.com>
2023-08-18 09:56:40 +0700

    Fix bug convert json for api remigpt

--- helpers/remigpt_api.py
+++ helpers/remigpt_api.py
@@ -11,9 +11,10 @@ class RemigptAPI:
 
     def get_banking_instruction(self, question):
         url = f"{self.base_url}/instruction"
-        response = requests.get(url, params={"question": question, "collection": "banking_instruction"})
+        params = {"question": question, "collection": "banking_instruction"}
+        response = requests.get(url, params=params)
 
         if response.status_code == 200:
-            return response.json()
+            return response.text
         else:
             response.raise_for_status()
