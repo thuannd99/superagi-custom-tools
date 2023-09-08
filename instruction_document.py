453bbad59e05dcb209e215f5ca8f48ec31fd313c
thuannd99 <thuannguyen@remitano.com>
2023-09-08 11:10:36 +0700

    Update instruction document

--- instruction_document.py
+++ instruction_document.py
@@ -5,12 +5,12 @@ from typing import Type
 from helpers.remigpt_api import RemigptAPI
 
 class InstructionDocumentInput(BaseModel):
-    question: str = Field(..., description="Human question")
+    question: str = Field(..., description="Whole requester's question or the goal, not your thought")
 
 class InstructionDocument(BaseTool):
     name: str = "Instruction Document Tool"
     args_schema: Type[BaseModel] = InstructionDocumentInput
-    description: str = "Tool you should go first to get instruction about a deposit"
+    description: str = "Tool you should go first to get instruction document"
 
     def _execute(self, question: str = None):
         base_url = self.get_tool_config("REMIGPT_URL")
